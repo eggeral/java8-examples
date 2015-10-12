5f817895355e3e5bc9dd4ddb4421bc048021ff6d
Alexander Egger <alexander.egger@gmail.com>
2015-10-13 00:55:56 +0200

    New examples for streams.

---
+++ src/main/java/b_stream/07-example_filter_min_max.java
@@ -0,0 +1,28 @@
+package b_stream;
+
+import java.util.stream.Stream;
+
+class FilterMinMax {
+    static class MeasurementPoint {
+        private final long timestamp;
+        private final double value;
+
+        MeasurementPoint(long timestamp, double value) {
+
+            this.timestamp = timestamp;
+            this.value = value;
+        }
+    }
+
+    public static void main(String[] args) {
+        //Get the timestamps of all values between a given min max.
+        Stream<MeasurementPoint> measurements = Stream.of(
+                new MeasurementPoint(1001, 50),
+                new MeasurementPoint(1002, 20),
+                new MeasurementPoint(1003, 11),
+                new MeasurementPoint(1004, 61),
+                new MeasurementPoint(1005, 32));
+
+        measurements.filter(m -> m.value > 15 && m.value < 55).map(m -> m.timestamp).forEach(System.out::println);
+    }
+}
--- src/main/java/b_stream/29-example_fibonacci.java
+++ src/main/java/b_stream/29-example_fibonacci.java
@@ -2,7 +2,7 @@ package b_stream;
 
 import java.util.stream.Stream;
 
-public class Fibu {
+class Fibonacci {
     public static void main(String[] args) {
         Stream.iterate(new long[] { 1, 1 }, p -> new long[] { p[1], p[0] + p[1] }).limit(92).forEach(p -> System.out.println(p[0]));
     }

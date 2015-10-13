98488392bca0fb13b692cf712fffaa0d2c954a76
Alexander Egger <alexander.egger@gmail.com>
2015-10-13 14:09:54 +0200

    Work with files in close with resources. Extract pattern for better readablity.

--- src/main/java/b_stream/16-example_longest_word_in_file.java
+++ src/main/java/b_stream/16-example_longest_word_in_file.java
@@ -4,13 +4,18 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 class LongestWordInFile {
 
     public static void main(String[] args) throws IOException {
-        Stream<String> lines = Files.lines(Paths.get("frankenstein.txt"));
-        System.out.println(lines.flatMap(l -> Arrays.stream(l.split("[\\P{L}]+")))
-                .max((lhs, rhs) -> Integer.compare(lhs.length(), rhs.length())).get());
+        Pattern wordSeparator = Pattern.compile("[\\P{L}]+");
+
+        try(Stream<String> lines = Files.lines(Paths.get("frankenstein.txt"))) {
+            System.out.println(lines.flatMap(l -> wordSeparator.splitAsStream(l))
+                    .max((lhs, rhs) -> Integer.compare(lhs.length(), rhs.length())).get());
+        }
+
     }
 }

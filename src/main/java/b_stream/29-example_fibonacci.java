b1b645d8ea5c5bda8031c4f3472f6e3e964ff78d
Alexander Egger <alexander.egger@gmail.com>
2015-10-13 22:05:08 +0200

    Renamed and reordered.

--- src/main/java/b_stream/29-example_fibonacci.java
+++
@@ -1,9 +0,0 @@
-package b_stream;
-
-import java.util.stream.Stream;
-
-class Fibonacci {
-    public static void main(String[] args) {
-        Stream.iterate(new long[] { 1, 1 }, p -> new long[] { p[1], p[0] + p[1] }).limit(92).forEach(p -> System.out.println(p[0]));
-    }
-}
